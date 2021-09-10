ea0724b1586af32f9093979437b5edbc997c9232
Amit <amitkr24031995@gmail.com>
2021-09-10 18:26:16 +0530

    Adding Stop Watch

---
+++ Logical Programs/Simulate Stopwatch Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+public class Demo
+{
+    public static void Main()
+    {
+        var sw = Stopwatch.StartNew();
+        long ticks = sw.ElapsedTicks;
+        Console.WriteLine(ticks);
+    }
+}
